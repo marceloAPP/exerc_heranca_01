3f4d39418ec354e2fa5e54f68e481a2c7581b700
marceloAPP <marcelo_freeday@yahoo.com.br>
2020-07-10 18:54:01 -0300

    primeira subD

---
+++ Exemp_126/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exemp_126
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
